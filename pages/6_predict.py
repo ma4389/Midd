1aa922d5eafdda5e3854538e94a72e09415a382c
ma4389 <149170027+ma4389@users.noreply.github.com>
2024-05-27 14:04:23 +0300

    6_predict.py

--- pages/6_predict.py
+++ pages/6_predict.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
